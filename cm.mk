564774285f6bd4b36a273901c0d92b1d710ac8e0
Jurica Vukadin <u.ra604@gmail.com>
2014-07-04 15:46:10 +0200

    mem: Inherit NFC bits

--- cm.mk
+++ cm.mk
@@ -20,17 +20,11 @@
 # definition file).
 #
 
-$(call inherit-product, device/htc/mem/full_mem.mk)
-
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-PRODUCT_RELEASE_NAME := mem
-
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/htc/mem/device.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/htc/mem/full_mem.mk)
 
 # Device naming
 PRODUCT_DEVICE := mem
@@ -39,4 +33,4 @@ PRODUCT_BRAND := htc
 PRODUCT_MODEL := One Mini 2
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mem BUILD_FINGERPRINT="htc/htc_europe/htc_memul:4.4.2/KOT49H/338202.2:user/release-keys" PRIVATE_BUILD_DESC="1.16.401.2 CL338202 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/htc_europe/htc_memul:4.4.2/KOT49H/338202.2:user/release-keys" PRIVATE_BUILD_DESC="1.16.401.2 CL338202 release-keys"
