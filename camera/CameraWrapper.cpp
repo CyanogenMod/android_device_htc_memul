603f6e358041cd69472938cc70590bfb7f8b4f4b
Jurica Vukadin <u.ra604@gmail.com>
2014-08-18 18:49:57 +0200

    memul: Update main camera specs

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -164,9 +164,9 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     /* Set sensor parameters */
     if (id == 0) {
-        params.set(android::CameraParameters::KEY_FOCAL_LENGTH, "3.82");
-        params.set(android::CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "69.6");
-        params.set(android::CameraParameters::KEY_VERTICAL_VIEW_ANGLE, "43.0");
+        params.set(android::CameraParameters::KEY_FOCAL_LENGTH, "3.81");
+        params.set(android::CameraParameters::KEY_HORIZONTAL_VIEW_ANGLE, "62.7");
+        params.set(android::CameraParameters::KEY_VERTICAL_VIEW_ANGLE, "48.7");
     }
     if (id == 1) {
         params.set(android::CameraParameters::KEY_FOCAL_LENGTH, "2.79");
