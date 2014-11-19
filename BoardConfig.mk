f14fc19c03fb9b2d3087ca29007fc157b50347b5
Jurica Vukadin <u.ra604@gmail.com>
2014-11-19 17:02:37 +0100

    memul: Set SELinux to permissive mode
    Change-Id: Ieffae9ab2512865269853ca43f74ed72e83c7c47

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/memul/include
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/memul/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
